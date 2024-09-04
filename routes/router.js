ec72874dcad7b556e352cc24988ea2627fb9f33c
Bruna Chaves <bcbs.brunachaves@gmail.com>
2024-09-04 14:26:40 +0200

    start maping the controller router

--- routes/router.js
+++ routes/router.js
@@ -1,5 +1,8 @@
 import { express } from "express";
-
+import { AppointmentController } from "./AppointmentController";
+import { DoctorController } from "./DoctorController.js";
+import { PacientController } from "./PacientController.js";
+import { PrescrptionController } from "./PrescrptionController.js";/
 let router = express.Router();
 
 router.get(
@@ -8,5 +11,10 @@ router.get(
         res.status(200).json({message: "HI!"});
     }
 );
+
+router.use("/", AppointmentController);
+router.use("/", DoctorController);
+router.use("/", PacientController);
+router.use("/", PrescrptionController);
 
 export default router;
